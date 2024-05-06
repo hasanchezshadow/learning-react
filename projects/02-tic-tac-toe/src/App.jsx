d9f47d7e8a2be4e721a09c035d0b4902e7244aed
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-06 13:50:42 +0300

    [Update] - Project 02 - Adding Css - Adding constants and reusable components - Adding logic to play the game and detect the winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+    X: 'x',
+    O: 'o',
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const WINNER_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const Square = ({children, updateBoard, index, isSelected}) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`;
+    const handleClick = () => {
+        updateBoard(index);
+    }
+    return (
+        <div className={className} key={index} onClick={handleClick}>
+            {children}
+        </div>
+    )
+};
+
+function App() {
+    // States
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [turn, setTurn] = useState(TURNS.X);
+    // null =  no winner, false = draw
+    const [winner, setWinner] = useState(null);
+
+    const checkWinner = (boardToCheck) => {
+        // We check all winning combinations to know if X or O won.
+        for (let combo of WINNER_COMBOS) {
+            const [a, b, c] = combo;
+            if (
+                boardToCheck[a] && // 0 -> X or O
+                boardToCheck[a] === boardToCheck[b] &&
+                boardToCheck[a] === boardToCheck[c]
+            ) {
+                return boardToCheck[a]; // X or O
+            }
+        }
+
+        // No winner
+        return null;
+    }
+
+    const updateBoard = (index) => {
+        // update not allowed
+        if (board[index] || winner) return;
+        // update board
+        const newBoard = [...board];
+        newBoard[index] = turn;
+        setBoard(newBoard);
+
+        // Check if we have a winner
+        const newWinner = checkWinner(newBoard);
+        if (newWinner) {
+            return setWinner(newWinner);
+
+        }
+
+        // update turn
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+        setTurn(newTurn);
+    };
+
+    return (
+        <main className={'board'}>
+            <h1>tic tac toe</h1>
+            <section className={'game'}>
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square
+                                key={index}
+                                index={index}
+                                updateBoard={updateBoard}
+                            >
+                                {_}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+
+            <section className={'turn'}>
+                <Square key={TURNS.X} isSelected={turn === TURNS.X}>
+                    {TURNS.X}
+                </Square>
+                <Square key={TURNS.O} isSelected={turn === TURNS.O}>
+                    {TURNS.O}
+                </Square>
+            </section>
+        </main>
+    )
 }
 
 export default App
