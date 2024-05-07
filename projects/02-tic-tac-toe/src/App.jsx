170cd373e3320053aa1205cf26f0ec0f0f8de8dd
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-07 13:20:11 +0300

    [Update] - Project 02 - Adding confetti when you get a winner - Creating and using function checkEndGame - Creating and using function resetGame - Creating and using modal to show the winner. - Installing new dependency canvas-confetti

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import './App.css'
+import confetti from "canvas-confetti";
 
 const TURNS = {
     X: 'x',
@@ -30,8 +31,9 @@ const Square = ({children, updateBoard, index, isSelected}) => {
 };
 
 function App() {
+    const initialData = Array(9).fill(null);
     // States
-    const [board, setBoard] = useState(Array(9).fill(null));
+    const [board, setBoard] = useState([...initialData]);
     const [turn, setTurn] = useState(TURNS.X);
     // null =  no winner, false = draw
     const [winner, setWinner] = useState(null);
@@ -53,6 +55,11 @@ function App() {
         return null;
     }
 
+    const checkEndGame = (boardToCheck) => {
+        // we can also use the function every
+        return !boardToCheck.includes(null);
+    }
+
     const updateBoard = (index) => {
         // update not allowed
         if (board[index] || winner) return;
@@ -64,18 +71,31 @@ function App() {
         // Check if we have a winner
         const newWinner = checkWinner(newBoard);
         if (newWinner) {
+            confetti();
             return setWinner(newWinner);
 
-        }
+        }  else if (checkEndGame(newBoard)) {
+            // game is over -> draw
+            setWinner(false);
+         }
 
         // update turn
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
         setTurn(newTurn);
     };
 
+    const resetGame = () => {
+        setBoard([...initialData]);
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+        setTurn(newTurn);
+        setWinner(null);
+
+    }
+
     return (
         <main className={'board'}>
             <h1>tic tac toe</h1>
+            <button onClick={resetGame}>Reset</button>
             <section className={'game'}>
                 {
                     board.map((_, index) => {
@@ -100,6 +120,25 @@ function App() {
                     {TURNS.O}
                 </Square>
             </section>
+
+            {
+                winner !== null && (
+                    <section className={'winner'}>
+                        <div className={'text'}>
+                            <h2>
+                                {winner === false ? 'Draw' : `The winner is `}
+                            </h2>
+                            <header className={'win'}>
+                                {winner && <Square>{winner}</Square>}
+                            </header>
+                            <footer>
+                                <button onClick={resetGame}>Restart
+                                </button>
+                            </footer>
+                        </div>
+                    </section>
+                )
+            }
         </main>
     )
 }
