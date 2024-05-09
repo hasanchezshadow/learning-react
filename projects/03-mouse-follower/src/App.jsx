6e4d68dbc721641aa08f00928db38652a2b8ba95
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-09 16:06:22 +0300

    [Update] - Project 03 Mouse Follower - Adding small changes to the index.css - Adding component follow mouse.

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,11 +1,59 @@
-import { useState } from 'react';
+import {useState, useEffect} from 'react';
 import './App.css'
 
-function App() {
+function FollowMouse() {
+    const [enabled, setEnabled] = useState(false);
+    const [position, setPosition] = useState({x: 0, y: 0});
 
-  return (
-    <h3>Project 3</h3>
-  )
+    useEffect(() => {
+        // follow mouse logic
+        const handleMove = (event) => {
+            const {clientX, clientY} = event;
+            console.log('handleMOve => ', {clientX, clientY})
+            setPosition({x: clientX, y: clientY});
+        };
+        if (enabled) {
+            // addEventListener('mousemove', handleMove);
+            addEventListener('pointermove', handleMove);
+        }
+
+        return () => {
+            removeEventListener('pointermove', handleMove);
+            setPosition({x: 0, y: 0});
+        }
+    }, [enabled]);
+
+    const handleClick = () => {
+        setEnabled(!enabled);
+    }
+
+    return (
+        <>
+            <div style={
+                {
+                    position: 'absolute',
+                    backgroundColor: '#09f',
+                    borderRadius: '50%',
+                    opacity: 0.8,
+                    pointerEvents: 'none',
+                    left: -20,
+                    top: -20,
+                    width: 40,
+                    height: 40,
+                    transform: `translate(${position.x}px, ${position.y}px)`
+                }
+            }/>
+            <button onClick={handleClick}>{enabled ? 'Disable' : 'Enable'} pointer tracking</button>
+        </>
+    );
+}
+
+function App() {
+    return (
+        <main>
+            <FollowMouse/>
+        </main>
+    )
 }
 
 export default App
