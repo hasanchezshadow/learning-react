d789bc88ef4583ed6e2183618083af16c8be0af8
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-09 19:57:58 +0300

    [Update] - Project 03 Mouse Follower - Creating new effect to change body properties when tracker is enabled

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -4,22 +4,31 @@ import './App.css'
 function FollowMouse() {
     const [enabled, setEnabled] = useState(false);
     const [position, setPosition] = useState({x: 0, y: 0});
-
+    // Pointer move effect
     useEffect(() => {
-        // follow mouse logic
+        // Follow pointer logic
         const handleMove = (event) => {
             const {clientX, clientY} = event;
-            console.log('handleMOve => ', {clientX, clientY})
             setPosition({x: clientX, y: clientY});
         };
         if (enabled) {
-            // addEventListener('mousemove', handleMove);
             addEventListener('pointermove', handleMove);
         }
 
         return () => {
             removeEventListener('pointermove', handleMove);
             setPosition({x: 0, y: 0});
+        }
+    }, [enabled]);
+    // Change cursor type effect (change body class)
+    useEffect(() => {
+        document.body.classList.toggle('no-cursor', enabled);
+
+        // cleanup method
+        // -> when component is unmounted
+        // -> when dependencies change, and before executing the effect again
+        return () => {
+            document.body.classList.remove('no-cursor');
         }
     }, [enabled]);
 
