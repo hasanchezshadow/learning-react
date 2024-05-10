099a53f7097ddee3532e04260deceaf4ad4e4c7d
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-10 16:58:29 +0300

    [Update] - Project 04 Technical Interview - Configuring React from scratch. - Installing and using standard lint.

---
+++ projects/04-react-prueba-tecnica/main.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <React.StrictMode>
+        <div>
+            <h1>Hello Vite!</h1>
+        </div>
+    </React.StrictMode>
+)
