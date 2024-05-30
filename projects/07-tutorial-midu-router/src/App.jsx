649893569e817f4a6764b719df5b4c196047e871
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-30 18:44:50 +0300

    [Update] - Project 07 Tutorial Midu Router - Changing file names. Adding Search component and route. - Installing and using path-ro-regex dependency.

--- projects/07-tutorial-midu-router/src/App.jsx
+++ projects/07-tutorial-midu-router/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import AboutPage from "./pages/AboutPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/About.jsx";
+import HomePage from "./pages/Home.jsx";
 import Page404 from "./pages/404.jsx";
+import SearchPage from "./pages/Search.jsx";
 
 import {Router} from "./components/Router.jsx";
 
@@ -13,6 +14,10 @@ const routes = [
     {
         path: '/about',
         component: AboutPage
+    },
+    {
+        path: '/search/:query',
+        component: SearchPage
     }
 ];
 
--- projects/07-tutorial-midu-router/src/components/Router.jsx
+++ projects/07-tutorial-midu-router/src/components/Router.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from "react";
 import {NAVIGATION_EVENT} from "../consts.js";
 
+import {match} from "path-to-regexp";
+
 export function Router({routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1>}) {
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
@@ -19,7 +21,26 @@ export function Router({routes = [], defaultComponent: DefaultComponent = () => 
         };
     }, []);
 
-    const Component = routes.find(({path}) => path === currentPath)?.component;
+    let routeParams = {};
 
-    return Component ? <Component/> : <DefaultComponent/>;
+    const Component = routes.find(({path}) => {
+        if (path === currentPath) return true;
+
+        // Using path-to-regex
+        // To detect dynamic routes like
+        // /search/:query <- :query is the dynamic route
+        const matcherUrl = match(path, {decode: decodeURIComponent});
+        const matched = matcherUrl(currentPath);
+        if (!matched) return false;
+
+        // Saving the dynamic url params
+        // that we have extracted with path-to-regex
+        // For example, if the route is /sear/:query
+        // and the url is /search/javascript
+        // matched.params.query === javascript
+        routeParams = matched.params;
+        return true;
+    })?.component;
+
+    return Component ? <Component routeParams={routeParams}/> : <DefaultComponent routeParams={routeParams}/>;
 }
