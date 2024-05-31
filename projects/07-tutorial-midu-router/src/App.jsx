275ac913cbdfeccd982133fe925afd6c0f77482c
ShadowWalker <sanchezhernan3@gmail.com>
2024-05-31 17:09:17 +0300

    [Update] - Project 07 Tutorial Midu Router - Lazy loading of Home and About pages. - Using Suspense with fallback to load routes.

--- projects/07-tutorial-midu-router/src/App.jsx
+++ projects/07-tutorial-midu-router/src/App.jsx
@@ -1,11 +1,18 @@
+import {lazy, Suspense} from "react";
+
 import './App.css'
-import AboutPage from "./pages/About.jsx";
-import HomePage from "./pages/Home.jsx";
+// import AboutPage from "./pages/About.jsx"; // Static import
+// import HomePage from "./pages/Home.jsx";
 import Page404 from "./pages/404.jsx";
 import SearchPage from "./pages/Search.jsx";
 
 import {Router} from "./components/Router.jsx";
 import {Route} from "./components/Route.jsx";
+
+
+// import('./pages/About.jsx').then(() => AboutPage); // Dynamic import
+const LazyHomePage = lazy(() => import('./pages/Home.jsx')); // Lazy import
+const LazyAboutPage = lazy(() => import('./pages/About.jsx')); // Lazy import
 
 const routes = [
     {
@@ -17,13 +24,15 @@ const routes = [
 function App() {
 
     return (
-        <main>
-            <Router routes={routes} defaultComponent={Page404}>
-                <Route path={'/'} component={HomePage}/>
-                <Route path={'/about'} component={AboutPage}/>
-            </Router>
-        </main>
-  )
+        <Suspense fallback={<div>Loading...</div>}>
+            <main>
+                <Router routes={routes} defaultComponent={Page404}>
+                    <Route path={'/'} component={LazyHomePage}/>
+                    <Route path={'/about'} component={LazyAboutPage}/>
+                </Router>
+            </main>
+        </Suspense>
+    )
 }
 
 export default App
