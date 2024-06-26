d7e196bab1c5ee2955c0f2fe3f8911a953192688
ShadowWalker <sanchezhernan3@gmail.com>
2024-06-26 17:27:31 +0300

    [Update] - Project 11 Technical test 55k
    [Done] - Getting 100 users
    
    - Configuring linter.
    - Avoiding timestamp files from gitignore.
    - Fetching users from the API.
    - Adding response models.

--- projects/11-technical-test-55k/src/App.tsx
+++ projects/11-technical-test-55k/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
+import {useGetUsers} from "./hooks/useGetUsers.ts";
+import {type User, type UserResponse} from '../models/user.model.ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {getUsers} = useGetUsers();
+    const [users, setUsers] = useState<User[]>([]);
+    useEffect(() => {
+        getUsers().then((response: UserResponse) => setUsers(response.results));
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <h1>Technical Test 55k</h1>
+                <p>Users: {users.length}</p>
+            </div>
+        </>
+    )
 }
 
 export default App
